b738b4f51cddf5b54a0783fc617a6300a924e6aa
prismaB <83291611+prismaB@users.noreply.github.com>
2021-10-02 00:11:29 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import os
 try:
     os.system("mv /home/" + os.getuid + " " + "xfeezer/ /usr/share/")
     os.system("pip3 install -r /usr/share/xfeezer/requiremets.txt")
+    os.system("python3 /usr/share/xfeezer/checkfiles.py")
     print("run python3 /usr/share/xfeezer/exxer.py")
 except:
     pass
